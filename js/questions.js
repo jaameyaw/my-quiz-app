f9238373ef15cae55bda1f6ee72ef74728cb9977
jaameyaw <johnsonameyaw22@gmail.com>
2025-05-08 21:48:59 +0000

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,35 @@
+'strict mode';
+
+const questions = [
+    {
+      question: "What time of day am I most productive?",
+      options: ["Early Morning", "Afternoon", "Evening", "Midnight"],
+      answer: "Midnight"
+    },
+    {
+      question: "If I could only eat one thing forever, what would it be?",
+      options: ["Pizza", "Banku & tilapia", "Jollof rice", "Fried rice"],
+      answer: "Jollof rice"
+    },
+    {
+      question: "What’s my love language?",
+      options: ["Words of affirmation", "Quality time", "Acts of service", "Physical touch"],
+      answer: "Physical touch"
+    },
+    {
+      question: "What am I most likely doing on a Sunday afternoon?",
+      options: ["Sleeping", "Coding 👨🏽‍💻", "At church", "Watching a movie"],
+      answer: "At church"
+    },
+    {
+      question: "How would I react if you surprised me with food?",
+      options: ["Smile and say thanks", "Give you a big hug", "Scream with joy 😍", "Pretend I’m not excited"],
+      answer: "Give you a big hug"
+    }
+  ];
+  
+
+const [question1, question2, question3, question4, question5] = questions;
+
+
+
