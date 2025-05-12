07045707f0a520750545cdf9daad9758f2fa645a
jaameyaw <johnsonameyaw22@gmail.com>
2025-05-12 01:18:47 +0000

    change questions arr to selectedQuestions arr

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ let questionText = document.querySelector('.question-text');
 let answerOptions = document.querySelectorAll('.answer-option');
 let nextQuestion = document.querySelector('.next-question-btn')
 let userHasClicked = false;
+let selectedQuestions;
 // Shuffles the questions array randomly using the Fisher-Yates algorithm.
 function fisherYatesShuffle(questionsArr) {
 
@@ -68,12 +69,12 @@ function checkUserClicked() {
 
 // Render question and options on the screen
 function displayQuestion(index) {
-    questionText.textContent = questions[index].question;
+    questionText.textContent = selectedQuestions[index].question;
 
     for (i = 0; i<answerOptions.length; i++) {
         let p = answerOptions[i].querySelector('p');
         let span = answerOptions[i].querySelector('span');
-        p.textContent = questions[index].options[i];
+        p.textContent = selectedQuestions[index].options[i];
 
          // Reset visual state
         answerOptions[i].classList.remove('correct', 'incorrect');
@@ -101,11 +102,11 @@ for (let i = 0; i < answerOptions.length; i++) {
         }
 
         // If correct
-        if (selectedText === questions[counterIndex].answer) {         
         this.classList.add('correct');
         span.textContent = 'check_circle';
         span.style.display = 'inline';
         correctCounter ++;
+        if (selectedText === selectedQuestions[counterIndex].answer) {
         } else {
             // If incorrect, show selected as wrong and find correct
             this.classList.add('incorrect');
@@ -115,7 +116,7 @@ for (let i = 0; i < answerOptions.length; i++) {
             for (k = 0; k < answerOptions.length; k++) {
                 let correctAns = answerOptions[k].querySelector('p').textContent;
                 let correctSpan = answerOptions[k].querySelector('span');
-                if (correctAns === questions[counterIndex].answer) {
+                if (correctAns === selectedQuestions[counterIndex].answer) {
                     answerOptions[k].classList.add('correct');
                     correctSpan.textContent = 'check_circle';
                     correctSpan.style.display = 'inline';
@@ -136,11 +137,11 @@ function displayResults() {
     let resultsMsg = resultContainer.querySelector('p'); 
 
     if (correctCounter === 5) {
-        resultsMsg.innerHTML = `🏆<b>${correctCounter}</b> out of <b>${questions.length}</b>. Ah, you be my twin or what? 😂`
+        resultsMsg.innerHTML = `🏆<b>${correctCounter}</b> out of <b>${selectedQuestions.length}</b>. Ah, you be my twin or what? 😂`
     } else if (correctCounter < 5 && correctCounter > 2) {
-        resultsMsg.innerHTML = `👏 <b>${correctCounter}</b> out of <b>${questions.length}</b>. Not bad! You know me quite well.`
+        resultsMsg.innerHTML = `👏 <b>${correctCounter}</b> out of <b>${selectedQuestions.length}</b>. Not bad! You know me quite well.`
     } else {
-        resultsMsg.innerHTML = `<b>${correctCounter}</b> out of <b>${questions.length}</b>. Ei! Do you even know me at all? 😂`
+        resultsMsg.innerHTML = `<b>${correctCounter}</b> out of <b>${selectedQuestions.length}</b>. Ei! Do you even know me at all? 😂`
     } 
 }
 
@@ -148,7 +149,7 @@ function displayResults() {
 // Move to next question and update status
 nextQuestion.addEventListener("click", function () {
     counterIndex++;
-    if (counterIndex < questions.length) {
+    if (counterIndex < selectedQuestions.length) {
         displayQuestion(counterIndex);
         checkAnswer();
         updateStatus();
